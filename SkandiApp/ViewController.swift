34ef2fa804d77d42a0aa20e5c31d94521b232c2a
OSIIOSdev <OSIIOS.dev@gmail.com>
2022-10-31 13:16:24 -0700

    Initial Commit

---
+++ SkandiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkandiApp
+//
+//  Created by OSIIOSdev on 31.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
